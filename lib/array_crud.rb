e58591718f96341c39e1bd128d334bec3d6f3792
Unsar Malik <unsar.malik24@gmail.com>
2019-05-06 20:53:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
  arrays = ["wow", "I", "am", "really", "learning"]
- arrays[]
+ arrays[2]
 end
 
 def retrieve_first_element_from_array(array)
