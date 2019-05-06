4610a3bb96ef1f8d8cdb5dd86174e5d6d193ba74
Unsar Malik <unsar.malik24@gmail.com>
2019-05-06 20:54:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,6 +37,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  arrays = ["wow", "I", "am", "really", "learning"]
+  arrays = ["wow", "I", "am", "really", "learning", "arrays!"]
   arrays[-1]
 end
