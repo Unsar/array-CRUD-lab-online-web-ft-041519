fbcbc521dfe0e26e79d1cd995644501edc52e66c
Unsar Malik <unsar.malik24@gmail.com>
2019-05-06 20:49:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   arrays = ["big", "fat", "small", "wide"]
-  arrays.shift("wow")
+  arrays.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
